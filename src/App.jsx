3e2a76f99c97a557a1b767637956e43c0df097fc
KashypHi <himanshu.ak205@gmail.com>
2024-06-11 13:24:03 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+  
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+          <h1>hello</h1>
+          <h2>hi</h2>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import reactdom from "react-dom/client"
 import App from "./App";
-let root = reactdom.createRoot(document.getElementById("root"));
+let  root = reactdom.createRoot(document.getElementById("root"));
 root.render(<App/>)
